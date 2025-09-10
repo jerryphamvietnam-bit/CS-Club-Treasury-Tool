cc48400caae15ad8400e810671ee1f0757a96b7a
jerryphamvietnam-bit <jerryphamvietnam@gmail.com>
2025-09-09 21:36:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 let transactions = [];
 const transactionsFile = path.join(__dirname, "transactions.json");
@@ -38,9 +38,4 @@ app.post("/api/transactions", (req, res) => {
   res.json({ status: "ok" });
 });
 
-// Catch-all to serve your front-end
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
